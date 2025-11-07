3ceb5d37e82bf31eb89de979923ed77baddee59b
Srivani Devaravajjala <srivani89@gmail.com>
2025-11-06 22:36:07 -0500

    Practices Variables concepts

---
+++ VariablesPractice.py
@@ -0,0 +1,11 @@
+name = "srivani"
+age = 36
+car_name = "Honda Acura"
+model_name = "RDX100"
+is_Petrol = True
+
+print(f"The type of name is {type(name)}")
+print(f"The age is: {age}")
+print(f"The car name is {car_name}")
+print(f"The car model is {model_name}")
+print(f"It is {is_Petrol} that it is a petrol car.")
