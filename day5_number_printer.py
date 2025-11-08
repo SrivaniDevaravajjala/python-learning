f8811060fc89a1416b8d01a3b1f23acbf079d9a2
Srivani Devaravajjala <srivani89@gmail.com>
2025-11-08 15:44:27 -0500

    Day 5: Prints numbers in a given range with for loop.

---
+++ day5_number_printer.py
@@ -0,0 +1,8 @@
+# Day 5 – Number Printer
+# Author: Sri
+
+for i in range(1, 11):
+    print(f"Number: {i}")
+
+# used formatted string literal in print function
+# above prints till 1 to (11-1): range (x,y) will print x to y-1;
