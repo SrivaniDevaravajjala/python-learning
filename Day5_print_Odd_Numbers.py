76f956c55d0c18c34f8effdddcc3ed65721d4a6d
Srivani Devaravajjala <srivani89@gmail.com>
2025-11-10 00:13:34 -0500

    odd even table prints with for loop

---
+++ Day5_print_Odd_Numbers.py
@@ -0,0 +1,11 @@
+#Print odd numbers
+num = 1
+for i in range (1, 30, 2):
+    print(f"{i} is odd")
+
+for i in range(2, 41, 2):
+    print(f"{i} is even")
+
+for i in range(1, 21):
+    for j in range(1,11):
+        print(f"{i} X {j} = {i * j}")
